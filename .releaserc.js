729a5aef2bfe2daffd78c775e3be236f3f408439
Hans Song <hans.m.song@gmail.com>
2022-06-23 01:08:27 +1000

    fix: remove semantic-release-plugin-debug

--- .releaserc.js
+++ .releaserc.js
@@ -4,7 +4,6 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@axalotl/semantic-release-plugin-debug",
     "@semantic-release/npm",
     "@semantic-release/github",
   ],
